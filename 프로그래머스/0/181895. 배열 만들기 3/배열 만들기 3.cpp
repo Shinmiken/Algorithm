bc2bd6638f1e12ec81be08a8adbfa82b51c56a98
Shinmiken <zxcvbnmsib@gmail.com>
2026-03-07 22:24:36 +0900

    [level 0] Title: 배열 만들기 3, Time: 20.06 ms, Memory: 19.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr, vector<vector<int>> intervals) 
+{
+    vector<int> answer;
+    
+    for(const auto &interval : intervals)
+    {
+        answer.insert(answer.end(), 
+                      arr.begin() + interval[0], 
+                      arr.begin() + interval[1] + 1);
+    }
+    
+    return answer;
+}
