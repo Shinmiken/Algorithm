0c611301ce7652410890c028047e27b8510090e1
Shinmiken <zxcvbnmsib@gmail.com>
2026-03-10 08:39:49 +0900

    [level 0] Title: 대문자로 바꾸기, Time: 0.02 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/0/181877. 대문자로 바꾸기/대문자로 바꾸기.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+#include <cctype>
+#include <algorithm>
+
+using namespace std;
+
+string solution(string myString) 
+{
+    transform(myString.begin(), myString.end(), myString.begin(), ::toupper);
+    
+    return myString;
+}
