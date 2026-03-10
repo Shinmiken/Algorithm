3c2d1b5b4ddd71895f9fa5d410fb38b1979a4e0c
Shinmiken <zxcvbnmsib@gmail.com>
2026-03-10 11:17:25 +0900

    [level 0] Title: A 강조하기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string myString) 
+{
+    for(char &c : myString)
+    {
+        if(c == 'a')
+        {
+            c = 'A';
+        }
+        else if(isupper(c) && c != 'A')
+        {
+            c = tolower(c); 
+        }
+    }
+    
+    return myString;
+}
