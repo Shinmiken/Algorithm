f6c575d647f41b1c355baf32bcc7fc01b3d855c1
Shinmiken <zxcvbnmsib@gmail.com>
2026-03-16 13:42:17 +0900

    [level 0] Title: 두 수의 합, Time: 0.65 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/0/181846. 두 수의 합/두 수의 합.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string solution(string a, string b) 
+{
+    string answer = "";
+    
+    int i = a.size() - 1;
+    int j = b.size() - 1;
+    
+    int carry = 0;
+    
+    while(i >= 0 || j >= 0 || carry)
+    {
+        int sum = carry;
+        
+        if(i>= 0) sum += a[i--] - '0';
+        if(j>= 0) sum += b[j--] - '0';
+        
+        answer += (sum % 10) + '0';
+        carry = sum / 10;
+    }
+    
+    reverse(answer.begin(), answer.end());
+    
+    return answer;
+}
