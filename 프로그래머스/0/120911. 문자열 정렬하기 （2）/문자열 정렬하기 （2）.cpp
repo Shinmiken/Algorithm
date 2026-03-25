5c4ca00690ac2800ff415e0dfbceeb64e5a72ad9
Shinmiken <zxcvbnmsib@gmail.com>
2026-03-26 08:42:16 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.01 ms, Memory: 3.63 MB -BaekjoonHub

---
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string solution(string my_string) 
+{
+    transform(my_string.begin(), my_string.end(), my_string.begin() 
+              ,[](char c) { return tolower(c); });
+    sort(my_string.begin(), my_string.end());
+    return my_string;
+}
