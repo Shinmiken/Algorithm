bb55745ae30894830c173cf6ecf50fdef3aebd75
Shinmiken <zxcvbnmsib@gmail.com>
2026-04-08 22:51:18 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int solution(int chicken) 
+{
+    int answer = 0;
+    int coupon = chicken;
+    
+    while (coupon >= 10)
+    {
+        int service = coupon / 10;
+        answer += service;
+        
+        coupon = coupon % 10 + service;
+    }
+    
+    return answer;
+}
